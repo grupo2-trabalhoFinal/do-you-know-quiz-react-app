6f332f248e4c4f80231b6e5db46d3b0de8c88b02
Macson Soares Rocha <92690565+maxdevsr@users.noreply.github.com>
2022-07-08 08:15:59 -0300

    Revert "Feature/category page"

--- do-you-know-react-app/src/Pages/CategoryPage/index.jsx
+++ do-you-know-react-app/src/Pages/CategoryPage/index.jsx
@@ -1,29 +1,19 @@
-import { useState } from "react";
 import ButtonCategory from "../../components/ButtonCategory";
 import TitleCategory from "../../components/TitleCategory";
 import { GlobalContainer } from "../../styles/global";
 import { ContainerListCategory, StyledCategoryPage } from "./style.jsx";
 
 const CategoryPage = () => {
-  const [listCategorys, setLisCategorys] = useState([
-    "HTML",
-    "CSS",
-    "JavaScript",
-    "React",
-    "Api",
-  ]);
   return (
     <GlobalContainer>
       <StyledCategoryPage>
         <TitleCategory>Categoria</TitleCategory>
         <ContainerListCategory>
-          {listCategorys.map((category, index) => {
-            return (
-              <ButtonCategory category={category} key={index}>
-                {category}
-              </ButtonCategory>
-            );
-          })}
+          <ButtonCategory>HTML</ButtonCategory>
+          <ButtonCategory>CSS</ButtonCategory>
+          <ButtonCategory>JavaScript</ButtonCategory>
+          <ButtonCategory>React</ButtonCategory>
+          <ButtonCategory>Api's</ButtonCategory>
         </ContainerListCategory>
       </StyledCategoryPage>
     </GlobalContainer>
