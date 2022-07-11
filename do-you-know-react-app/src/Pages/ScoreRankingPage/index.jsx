9d21e21cb96558ad282f3cbf798654063ed2cfe1
Ramon souza <ramonkyo@gmail.com>
2022-07-11 17:25:36 -0400

    fix: bug render

--- do-you-know-react-app/src/Pages/ScoreRankingPage/index.jsx
+++ do-you-know-react-app/src/Pages/ScoreRankingPage/index.jsx
@@ -1,19 +1,34 @@
 import background from "../../Assets/background.svg"
-import BackgroundImg from "./style";
+import BackgroundImg, { RetryButton } from "./style";
+import Logo from "../../Assets/LOGO.svg"
+
+
+
 
 const ScoreRankingPage = () => {
+
+
+  
   return(
-    
-        <BackgroundImg>
-          <img src={background} alt="" />
-        </BackgroundImg>
-          
         
+          <BackgroundImg>
+            <img src={background} alt="Logo Do You Know" ></img>
+
+            <div>
+              <img src={Logo}></img>
+            </div>
+            
+            <label></label>
+            
+            <RetryButton>
+              <p>Voltar</p>
+            </RetryButton>
+            
+          
+          </BackgroundImg>
           
   );
 };
 
 export default ScoreRankingPage;
-
-
 
--- do-you-know-react-app/src/Pages/ScoreRankingPage/style.js
+++ do-you-know-react-app/src/Pages/ScoreRankingPage/style.js
@@ -1,13 +1,53 @@
 import styled from "styled-components";
-import Logo from "../../Assets/LOGO.svg"
 
 
 export const BackgroundImg = styled.div`
 
-background: url(${Logo});
+background-repeat: no-repeat;
+background-size: cover;
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: space-evenly;
+
+
+
+min-width: 100vw;
+height: 100vh;
+
+div {
+    background-size: cover;
+    margin-top: -3250px;
+    margin-right: 65px;
+}
+
 `;
 
 export default BackgroundImg;
+
+
+
+
+export const RetryButton = styled.button`
+
+font-size: 1.1875rem;
+  font-weight: 300;
+  line-height: 1.75rem;
+
+  text-align: center;
+  padding: 0.1875rem 2.6875rem;
+
+  border: none;
+  border-radius: 0.875rem;
+
+  background: ${(props) => props.backgroundColorButton || "#1E4E9D"};
+  color: ${(props) => props.color || "#fff"};
+  
+`
+
+
+
+
 
 
 
