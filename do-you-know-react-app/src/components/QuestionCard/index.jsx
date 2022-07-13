7731985aaa3f03b234d0a82ff401b60279bcef46
Ramon souza <ramonkyo@gmail.com>
2022-07-12 20:55:47 -0400

    fix: random-questions

--- do-you-know-react-app/src/components/QuestionCard/index.jsx
+++ do-you-know-react-app/src/components/QuestionCard/index.jsx
@@ -1,12 +1,66 @@
+
+import { useEffect } from "react";
+import { useState } from "react";
+import { StyledAnswerButton } from "../ButtonAnswer/style";
 import { StyledQuestionCard } from "./style";
 
-export const QuestionCard = ({ questions }) => {
-  return questions.map((question) => (
-    <StyledQuestionCard>
-      <h3>{question.questionText}</h3>
-      {question.answerOptions.map((answer) => (
-        <button>{answer.answerText}</button>
-      ))}
-    </StyledQuestionCard>
-  ));
+export const QuestionCard = ({ question, fowardQuestion, clicked }) => {
+
+  const [currentsQuestions, setCurrentsQuestions] = useState(question.answerOptions)
+  
+  // let randomAnswer = []
+  // for (let i = question.answerOptions.length - 1; i > 0; i--) {
+  //   const j = Math.floor(Math.random() * (i + 1));
+  //   [question.answerOptions[i], question.answerOptions[j]] = [question.answerOptions[j], question.answerOptions[i]];
+  //   console.log(question.answerOptions[j])
+  // }
+  // console.log(question.answerOptions)
+  
+
+  useEffect(() => {
+    
+      setCurrentsQuestions(shuffleArray(question.answerOptions))
+    
+  }, [question])
+
+  function shuffleArray(arr) {
+    console.log(arr)
+    for (let i = question.answerOptions.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+    console.log(arr)
+      return arr;
+}
+
+  
+  return (
+    <>
+      {
+        <StyledQuestionCard>
+          <h3>{question.questionText}</h3>
+          
+          {currentsQuestions.map((answer) => {
+            const isCorrect = answer.isCorrect;
+
+            return (
+              <StyledAnswerButton
+                key={answer.answerText}
+                isCorrect={isCorrect}
+                clicked={clicked}
+
+                onClick={(event) =>  {
+                  
+                  fowardQuestion();
+                }}
+              >
+                {answer.answerText}
+              </StyledAnswerButton>
+            );
+          })}
+        </StyledQuestionCard>
+      }
+    </>
+  );
 };
+
